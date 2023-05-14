8806dd068adf85d666a13202daa96c652dc7ad88
Roy Novoselsky <84639293+roynov01@users.noreply.github.com>
2023-05-14 10:39:01 +0300

    Add files via upload

--- sc_functions.R
+++ sc_functions.R
@@ -178,7 +178,9 @@ plot_Enricher <- function(enrich.hallmark, qval_color="red",title="") {
     guides(fill=guide_legend(title="-log(Qval)",reverse=T))
 }
 
-GSEA_function <- function(gene_list,log2fc_ratio, geneset="hallmark",organism="Mus musculus",score_type="std", qval_threshold=1, nperm=1000) {
+GSEA_function <- function(gene_list,log2fc_ratio, geneset="hallmark",
+                          organism="Mus musculus",score_type="std", qval_threshold=1,
+                          nperm=1000) {
   # https://bioinformatics-core-shared-training.github.io/RNAseq_May_2020_remote/html/06_Gene_set_testing.html
   # https://bioconductor.org/packages/devel/bioc/manuals/fgsea/man/fgsea.pdf 
   # 
@@ -190,6 +192,7 @@ GSEA_function <- function(gene_list,log2fc_ratio, geneset="hallmark",organism="M
   #' Also can be a vector of category and subcategory, such as c("C2","KEGG"). 
   #' or a dataframe of two columns - first is the genesets, second is the gene
   #' @param organism either "Mus musculus" or "Homo sapiens"
+  #' @param changes 
   #' @return Returns a dataframe with all relevent results. enriched in  gene set
   if (geneset=="hallmark"){
     hallmark = msigdbr(species=organism,category="H" )
@@ -210,12 +213,12 @@ GSEA_function <- function(gene_list,log2fc_ratio, geneset="hallmark",organism="M
   names(log2fc_ratio) = gene_list
   hallmark.genes = hallmark[,c("gs_name", "gene_symbol")]
   
-  hallmark.list <- hallmark.genes %>% 
+  hallmark.list = hallmark.genes %>% 
     group_by(gs_name) %>% 
     summarise(all.genes = list(unique(gene_symbol))) %>% 
     deframe()
   
-  enrich.hallmark <- fgseaSimple(pathways = hallmark.list,
+  enrich.hallmark = fgseaSimple(pathways = hallmark.list,
                                  stats = log2fc_ratio,
                                  scoreType = score_type,
                                  nperm=1000)
@@ -230,7 +233,7 @@ GSEA_function <- function(gene_list,log2fc_ratio, geneset="hallmark",organism="M
   enrich.hallmark$num_of_genes = NA
   for (row in 1:nrow(enrich.hallmark)){enrich.hallmark$num_of_genes[row] = length(enrich.hallmark$leadingEdge[row][[1]])}
   
-  enrich.hallmark$direction="upregulated"
+  enrich.hallmark$direction = "upregulated"
   enrich.hallmark$direction[enrich.hallmark$NES<0] = "downregulated"
   enrich.hallmark$gene_ratio = enrich.hallmark$num_of_genes/enrich.hallmark$size
   return(list(GSEA=enrich.hallmark,pathways=hallmark.list))
@@ -257,18 +260,19 @@ plot_GSEA_dotplot = function(enrich.hallmark,title="GSEA") {
     guides(size=guide_legend(title="Gene ratio"),color=F, alpha=guide_legend(title="Adjusted P-value"))+
     ylab(NULL) + xlab("Normalized enrichment score (NES)")+
     ggtitle(title) +
-    facet_grid(.~direction) +
+    # facet_grid(.~direction,scales = "free") +
+    facet_wrap(.~direction,scales = "free_x") +
     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 }
 
-export_GSEA_csv = function(GSEA,output_file) {
+export_GSEA_csv = function(GSEA,output_file=NULL) {
   temp = GSEA
   temp$genes = ""
   for (i in 1:nrow(temp)){
     temp$genes[i] = paste(unlist(temp$leadingEdge[i]),collapse = '_')
   }
   temp$leadingEdge = NULL
-  write.csv(temp,output_file)
+  if (!is.null(output_file)){write.csv(temp,output_file)}
   return(temp)
 }
 
@@ -291,9 +295,12 @@ export_for_pie = function(genes, values, path="") {
   #' @param path if provided - saves the data into file
   #' 
   translation = read.csv("X:\\roy\\resources\\Ensemble\\refseq_peptide_genes.csv")
+  # translation = read.csv("X:\\roy\\resources\\Ensemble\\ensemble_entrez_geneId_conversion.csv")
   df = data.frame(genes,values)
   colnames(df) = c("external_gene_name","value")
+   
   df_merged = merge(df,translation[,c("external_gene_name","refseq_peptide")],by="external_gene_name")
+
   df_merged = df_merged[df_merged$refseq_peptide != "",]
   
   dt = data.table(df_merged)
@@ -318,4 +325,223 @@ averege_duplicated_rows = function(df, column_containing_duplicates) {
   return (aggregate( . ~ eval(parse(text=column_containing_duplicates)), df, mean, na.rm=T, na.action=NULL))
 }
 
+max_duplicated_rows = function(df, column_containing_duplicates) {
+  new_df = data.frame(matrix(NA, nrow=0, ncol=ncol(df)))
+  colnames(new_df) = colnames(df)
+  for(i in 1:nrow(df)){
+    gene = df[i,column_containing_duplicates]
+    if (!(gene %in% new_df[,column_containing_duplicates])){
+      new_df[nrow(new_df) + 1,] = df[i,]
+      rownames(new_df)[nrow(new_df)] = gene
+      next
+    }
+    cur_sum = sum(new_df[gene,colnames(new_df) != column_containing_duplicates])
+    new_sum = sum(df[i,colnames(df) != column_containing_duplicates])
+    if (new_sum > cur_sum){
+      new_df[gene,] = df[i,]
+    }
+  }
+  return(new_df)
+}
+
+########### intestinal plots and markers ############################
+
+plot_marker = function(expression_mat, genes, celltypes=NA) {
+  #' creates barplot of selected genes based on signature matrix
+  #' 
+  #' @param expression_mat signature matrix (rows are genes, columns are celltypes)
+  #' @param genes vector of gene names
+  #' @param celltypes vector of gene celltypes which would be highlighted by color
+  plots = list()
+  expression = expression_mat[genes,]
+  expression$gene = rownames(expression)
+  expression = pivot_longer(expression,cols=colnames(expression)[colnames(expression) != "gene"])
+  expression$color = F
+  expression$color[expression$name %in% celltypes] = T
+  
+  for (i in 1:length(genes)) {
+    plot = expression[expression$gene==genes[i],]
+    p = ggplot(plot,aes(name,value,fill=color)) + geom_bar(stat="identity")+
+      coord_flip() +
+      theme_bw() + 
+      xlab("") + ylab("") +
+      scale_fill_manual(values=c("blue","red")) +
+      theme(legend.position="none") + 
+      ggtitle(genes[i])
+    plots[[i]] = p
+  }
+  return (plots)
+}
+
+plot_inna = function(genes, organism="mouse",celltypes=NA){
+  #' plots expression in celltypes
+  #' #' @param genes gene or genes to plot
+  #' #' @param organism "human" or "mouse".
+  #' #' @param celltypes vector of gene celltypes which would be highlighted by color
+  genes = toupper(genes)
+  if (organism=="human") {filename = "X:\\roy\\apicome\\visium_export_from_yotam\\P13cell_type_signature_matrix.csv"}
+  if (organism=="mouse") {filename = "X:\\roy\\resources\\datasets\\innas_data.csv"}
+  expression = read.csv(filename,row.names=1)
+  # expression = expression[!grepl("^AC\\d|orf|^RP[SL]|^AP\\d|AL\\d|^LINC",rownames(expression)),]
+  # expression = expression[!grepl("^Gm\\d|^BC|^Rp[sl]|Rik|^AI\\d|^AA",rownames(expression)),]
+  rownames(expression) = toupper(rownames(expression))
+  barplots = plot_marker(expression, genes,celltypes=celltypes)
+  
+  x = sapply(barplots, function(p, add){return(p+ggtitle(paste(p$lebels$title," (",organism,")",sep="")))}, add=organism)
+  
+  for (i in 1:length(barplots)){barplots[[i]] = barplots[[i]] + ggtitle(paste(barplots[[i]]$labels$title," (",organism,")",sep=""))}
+  return(barplots)
+}
+
+find_markers = function(celltypes=NA,organism="human",celltype_name=NA,RATIO_THRESH=2,EXP_THRESH=10^-6,barplots=F) {
+  #' Find and plot markers of intestine celltypes.
+  #' to see available cell-types, run the function with organism variable only. then run it with celltype(s)
+  #' 
+  #' @param celltypes character vector of cell types. to see available cell-types, run the function with organism variable only.
+  #' @param organism "human" or "mouse".
+  #' @param celltype_name str. not necessary, used for title plot etc.
+  #' @param RATIO_THRESH float. threshold of expression, above which markers will be returned.
+  #' @param EXP_THRESH float. ratio threshold, above which markers will be returned.
+  #' @param barplots bool. return a list of barplots for each marker?
+  #' @return Returns vector of markers, expression matrix of the markers and plots
+  if (organism=="human") {
+    filename = "X:\\roy\\apicome\\visium_export_from_yotam\\P13cell_type_signature_matrix.csv"}
+  if (organism=="mouse") {filename = "X:\\roy\\resources\\datasets\\innas_data.csv"}
+  expression = read.csv(filename,row.names=1)
+  if (typeof(celltypes)=="logical") {return(colnames(expression))}
+  if (is.na(celltype_name)){celltype_name = celltypes[1]}
+  
+  expression = expression[!grepl("^AC\\d|orf|^RP[SL]|^AP\\d|AL\\d|^LINC",rownames(expression)),]
+  expression = expression[!grepl("^Gm\\d|^BC|^Rp[sl]|Rik|^AI\\d|^AA",rownames(expression)),]
+  
+  chosen_cell = data.frame(expression[,celltypes])
+  rownames(chosen_cell) = rownames(expression)
+  colnames(chosen_cell) = celltypes
+  
+  other = expression[,!(colnames(expression) %in% celltypes)]
+  
+  chosen_avg = apply(chosen_cell,1,mean)
+  other_max = apply(other,1,max)
+  
+  comparison = data.frame(chosen_avg,other_max)
+  comparison$other_max[comparison$other_max==0] = 1e-6 # pseudonum
+  
+  comparison$ratio = comparison$chosen_avg/comparison$other_max
+  comparison$fc_signif = comparison$ratio > RATIO_THRESH
+  comparison$expressed = comparison$chosen_avg > EXP_THRESH
+  comparison$signif = comparison$fc_signif & comparison$expressed
+  
+  plot = comparison
+  plot$gene = rownames(plot)
+  plot[c("chosen_avg","other_max")] = log(plot[c("chosen_avg","other_max")],10)
+  p = ggplot(plot,aes(x=chosen_avg, y=other_max,colour=signif)) + geom_point() + 
+    ggtitle(paste("Genes that are unique to ",celltype_name,sep=""))+
+    geom_text_repel(plot[plot$signif==T,], mapping=aes(x=chosen_avg, y=other_max,label=gene),max.overlaps = 30)+
+    theme_bw() + ylab("log10(max expression in other cells)") + xlab("log10(averege expression in celltype)")+
+    theme(legend.position = "none",panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+    scale_color_manual(values = c("grey", "red"))
+  
+  res = comparison[rownames(comparison)[comparison$signif==T],]
+  res = res[order(res$ratio*res$chosen_avg,decreasing=T),c("chosen_avg","other_max","ratio")]
+  celltype_markers = rownames(res)
+  
+  if (barplots==T) {
+    barplots = plot_marker(expression, celltype_markers, celltypes)
+  }
+  
+  return (list(markers=celltype_markers,markers_tbl=res,celltypes=celltypes,expression_mat=expression,plot=p, barplots=barplots))
+}
+
+plot_human_sections = function(gene){
+  #' plots expression along the intestinal segments in human data from Rachel Zwick data in mouse or human
+  #' @param gene string, gene to plot
+  gene = toupper(gene)
+  path = "X:\\Shalevi\\Rachel_Zwick"
+  files = c("Enteroendocrine cells.xlsx",
+            "Goblet cells.xlsx",
+            "Tuft cells.xlsx",
+            "Paneth cells.xlsx",
+            "Crypt cells.xlsx",
+            "Villus cells.xlsx")
+  
+  for(i in 1:length(files)){
+    cur_path = paste(path,files[i],sep="/")
+    name = sub(" cells.xlsx","",files[i])
+    d = read_excel(cur_path)
+    d = d[1:6]
+    d_long = pivot_longer(d,cols=colnames(d[2:6]),names_to="section",values_to=name)
+    if (i == 1){
+      sections = d_long
+    } else {
+      sections[,name] = d_long[,name]
+    }
+  }
+  sections$section = as.integer(gsub("mean ","",sections$section))
+  plot = sections[sections["Gene name"] == gene,]
+  plot = pivot_longer(plot,colnames(plot)[3:ncol(plot)], names_to = "celltype", values_to = "expression")
+  ggplot(data=plot,aes(x=section,y=expression,color=celltype)) +
+    geom_line(size=2) +
+    theme_bw() + theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank()) +
+    scale_color_manual(values=c("purple","lightblue","orange","darkgreen","blue","red")) +
+    theme(legend.position="right") + 
+    # theme(legend.position=c(0.77,0.9),legend.direction="horizontal",legend.title=element_blank()) +
+    ggtitle("intestine axis") + xlab("") + ylab("")
+}
+
+plot_tip_base = function(gene,organism="mouse"){
+  #' plots tip/base expression in mouse or human
+  #' @param gene string, gene to plot
+  #' @param organism "human" or "mouse".
+  gene = toupper(gene)
+  if(organism=="mouse"){
+    apicome = read.csv("X:\\roy\\apicome\\analysis\\LCM_bulk\\outputs_apr23\\non_filtered/medians.csv")
+  }
+  if (organism=="human"){
+    apicome = read.csv("X:\\roy\\apicome\\analysis\\LCM_human\\outputs_apr23\\non_filtered/medians.csv")
+  }
+  apicome$X=NULL
+  apicome$gene = toupper(apicome$gene)
+  apicome$tip = (apicome$apical_tip+apicome$basal_tip)/2
+  apicome$base = (apicome$basal_base+apicome$basal_base)/2
+  
+  plot = apicome[apicome$gene %in% gene,c("gene","tip","base")]
+  plot = pivot_longer(plot,colnames(plot)[2:3], names_to = "villus_zone", values_to = "expression")
+  ggplot(data=plot,aes(x=villus_zone,y=expression)) +
+    geom_bar(stat="identity",fill=c("pink","blue"),color="black",size=1) +
+    theme_bw() + theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank()) +
+    xlab("") + ggtitle("Villus zone") + ylab("")
+}
+
+plot_apicome = function(gene,organism="mouse"){
+  #' plots apicome in mouse or human
+  #' @param gene string, gene to plot
+  #' @param organism "human" or "mouse".
+  gene = toupper(gene)
+  if(organism=="mouse"){
+    apicome = read.csv("X:\\roy\\apicome\\analysis\\LCM_bulk\\outputs_apr23\\non_filtered/medians.csv")}
+  if (organism=="human"){
+    apicome = read.csv("X:\\roy\\apicome\\analysis\\LCM_human\\outputs_apr23\\non_filtered/medians.csv")}
+  apicome$X=NULL
+  apicome$gene = toupper(apicome$gene)
+  apicome$apical = (apicome$apical_tip+apicome$apical_base)/2
+  apicome$basal = (apicome$basal_tip+apicome$basal_base)/2
+  plot = apicome[apicome$gene %in% gene,c("gene","apical","basal")]
+  plot = pivot_longer(plot,colnames(plot)[2:3], names_to = "apicome", values_to = "expression")
+  ggplot(data=plot,aes(x=apicome,y=expression)) +
+    geom_bar(stat="identity",fill=c("lightgreen","yellow"),color="black",size=1) +
+    theme_bw() + theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank()) +
+    xlab("") + ggtitle("Apicome") + ylab("")
+}
+
+plot_intestines = function(gene,organism="mouse"){
+  #' plots apicome, villus axis, intestinal axis and expression
+  #' @param gene string, gene to plot
+  #' @param organism "human" or "mouse".
+  p1 = plot_inna(gene, organism=organism)
+  p2 = plot_human_sections(gene)
+  p3 = plot_tip_base(gene,organism=organism)
+  p4 = plot_apicome(gene,organism=organism)
+  p = ggarrange(p1[[1]],ggarrange(p2,p3,p4, nrow = 3),ncol = 2) 
+  return(p)
+}
 
